a2fddac20ca6b31e3d83a9e49514db19c7292f1f
Young-Jun-Park18 <dudwns4619@naver.com>
2025-03-12 03:24:26 +0900

    fontAwesomeIcon에서 아이콘 추가 완료

--- dna/src/App.jsx
+++ dna/src/App.jsx
@@ -2,6 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // Font Awesome 컴포넌트 가져오기
+import { faHexagonNodes } from "@fortawesome/free-solid-svg-icons"; // 사용할 아이콘 가져오기
+
+
+{/* 
+    아이콘 : Font-awesome 이용
+    출처 : https://fontawesome.com/search?q=chart&o=r&ic=free&s=solid&ip=classic
+    이용 방법1 : 터미널에 패키지설치 -> npm install @fortawesome/fontawesome-svg-core @fortawesome/free-solid-svg-icons @fortawesome/react-fontawesome
+    이용 방법2 : 컴포넌트 import -> import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+    이용 방법3 : 내가 쓸 아이콘 import -> import { faHexagonNodes } from "@fortawesome/free-solid-svg-icons";
+    이용 방법4 : return() 함수 안에서 컴포넌트 추가 -> <FontAwesomeIcon icon={faHexagonNodes} size="2x" color="blue" />
+  */}
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,7 +23,11 @@ function App() {
      {/* ----------header---------- */}
      <div id = 'header'>
         <div class = "hearderInner">
-          <div className = 'logo'>
+          <div className = 'icon'>
+            {/* <FontAwesomeIcon/> 이것은 컴포넌트 입니다. size = "nx" 속성으로 n배 키울 수 있고, color = ""로 색상 변경 가능*/}
+            <FontAwesomeIcon icon={faHexagonNodes} size="3x"/>
+          </div>
+          <div className = 'pgName'>
             DNA
           </div>
           <nav className = 'navBar'>
