a9effadc9bf439ba5e97ce927a157e3136e8ece3
Young-Jun-Park18 <dudwns4619@naver.com>
2025-04-11 17:53:48 +0900

    professorPhoto까지 완료

--- dna/src/pages/members/Professor/Professor.jsx
+++ dna/src/pages/members/Professor/Professor.jsx
@@ -3,9 +3,23 @@ import styles from "./Professor.module.css";
 function Professor(){
   return(
     <>
-      <div className={styles.a}>교수 소개란</div>
-    
-    
+      <div className= {styles.professorWrap}>
+        <div className= {styles.professorBox}>
+          <div className= {styles.professorTitle}>
+            <h2>Professor</h2>
+            <div className= {styles.professorLine1}></div>
+            <div className= {styles.professorLine2}></div>
+            <div className={styles.professorIntroWrap}>
+              <div className={styles.professorPhoto}>
+                <img src="/profH.jpg" alt="박현희 교수님 사진" />
+              </div>
+              <div className={styles.professorIntro}>
+                내용이 추가될 예정입니다
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
