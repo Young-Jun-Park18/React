029ef6550dabb313e7139fbf8f84cbeb39ced18b
Young-Jun-Park18 <dudwns4619@naver.com>
2025-04-11 18:35:58 +0900

    animationKey로 동일 컴포넌트 내에서 애니메이션효과 계속 주기

--- dna/src/pages/members/Members.jsx
+++ dna/src/pages/members/Members.jsx
@@ -1,10 +1,21 @@
 import styles from './Members.module.css';
-import {Routes, Route, Link, Outlet, useLocation } from 'react-router-dom';
 // useLocation은 현재 경로를 확인할 수 있게 도와주는 라이브러리
+import { Routes, Route, Link, Outlet, useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
 
 function Members(){
+  // 현재 내 위치를 확인해주는 변수
+  const location = useLocation();
 
-  const location = useLocation(); /// 현재 내 위치를 확인해주는 변수
+  // 경로가 바뀔 때마다 key를 변경해서 컴포넌트 재렌더링 유도
+  const [animationKey, setAnimationKey] = useState(0);
+  useEffect(() => {
+   
+    setAnimationKey(prev => prev + 1);
+    // 만약 location.pathname = /Members/Professor 인데 이게 바뀌면 useEffect가 실행됨
+  }, [location.pathname]);
+
 
   return(
     <>
@@ -32,7 +43,7 @@ function Members(){
           </ul>
         </div>
       </nav>
-      <h2 className={styles.membersTitleText}>Members</h2>
+      <h2 className={styles.membersTitleText} key={animationKey}>Members</h2>
 
 
 
